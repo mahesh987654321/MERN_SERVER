6000985ec80abe40ed00ca35824059d3a21dfc4d
Mahesh Biswas <maheshbiswas26@gmail.com>
2022-09-03 13:29:27 +0600

    bycript

--- router/auth.js
+++ router/auth.js
@@ -5,32 +5,57 @@ const User = require("../model/userSchema");
 router.get("/", (req, res) => {
   res.send("Home page server");
 });
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   const { name, email, phone, work, password, cpassword } = req.body;
 
   if (!name || !email || !phone || !work || !password || !cpassword) {
     return res.status(422).json({ error: "Please fill the field properly" });
   }
-  User.findOne({ email: email })
-    .then((userExists) => {
-      if (userExists) {
-        return res
-          .status(409)
-          .json({ error: "Ohh dear user is already exists" });
-      }
-      const user = new User({ name, email, phone, work, password, cpassword });
-      user
-        .save()
-        .then(() => {
-          res.status(201).json({ message: "User is created" });
-        })
-        .catch((err) => {
-          res.status(500).json({ err: "Failed to register try again" });
-        });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+  try {
+    const userExists = await User.findOne({ email: email });
 
+    if (userExists) {
+      return res.status(409).json({ error: "Ohh dear user is already exists" });
+    }
+    const user = new User({
+      name,
+      email,
+      phone,
+      work,
+      password,
+      cpassword,
+    });
+    const userRegister = await user.save();
+    if (userRegister) {
+      res.status(201).json({ message: "User is created" });
+    } else {
+      res.status(500).json({ err: "Failed to register try again" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(422).json({ error: "Please fill the field properly" });
+  }
+  try {
+    const userExists = await User.findOne({ email: email });
+    if (userExists) {
+      return res.status(201).json({ message: "Login successful" });
+    } else {
+      res.status(500).json({ error: "Email is not found" });
+    }
+    const user = new User({ email, password });
+    const userLogin = await user.save();
+    if (userLogin) {
+      res.status(201).json({ message: "User is created" });
+    } else {
+      res.status(500).json({ err: "Failed to register try again" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
 module.exports = router;
